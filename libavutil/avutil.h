012dd29f23476ebc1ab448be516504d926b28e7c
hearther <hearther.lin@gmail.com>
2017-08-02 10:05:02 +0800

    lost in last commit

--- libavutil/avutil.h
+++ libavutil/avutil.h
@@ -190,7 +190,7 @@ const char *avutil_license(void);
  * @brief Media Type
  */
 
-enum AVMediaType {
+enum FFMpegAVMediaType {
     AVMEDIA_TYPE_UNKNOWN = -1,  ///< Usually treated as AVMEDIA_TYPE_DATA
     AVMEDIA_TYPE_VIDEO,
     AVMEDIA_TYPE_AUDIO,
