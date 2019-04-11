40b438a95d6a9e466edbc48b33608c3bb4e4a9a1
hearther <hearther.lin@gmail.com>
2019-04-11 19:30:41 +0800

    lost in last

--- libavfilter/buffersink.c
+++ libavfilter/buffersink.c
@@ -186,7 +186,7 @@ type av_buffersink_get_##field(const AVFilterContext *ctx) { \
     return ctx->inputs[0]->field; \
 }
 
-MAKE_AVFILTERLINK_ACCESSOR(enum AVMediaType , type               )
+MAKE_AVFILTERLINK_ACCESSOR(enum FFMpegAVMediaType , type               )
 MAKE_AVFILTERLINK_ACCESSOR(AVRational       , time_base          )
 MAKE_AVFILTERLINK_ACCESSOR(int              , format             )
 
